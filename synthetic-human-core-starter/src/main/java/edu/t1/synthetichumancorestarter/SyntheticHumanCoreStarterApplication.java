856e703fa9f1499f72282a37ba71b795648f10c9
melonegoist <408517@niuitmo.ru>
2025-07-18 20:38:54 +0300

    initial commit

---
+++ synthetic-human-core-starter/src/main/java/edu/t1/synthetichumancorestarter/SyntheticHumanCoreStarterApplication.java
@@ -0,0 +1,13 @@
+package edu.t1.synthetichumancorestarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntheticHumanCoreStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyntheticHumanCoreStarterApplication.class, args);
+    }
+
+}
---
+++ synthetic-human-core-starter/src/test/java/edu/t1/synthetichumancorestarter/SyntheticHumanCoreStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.t1.synthetichumancorestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntheticHumanCoreStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
