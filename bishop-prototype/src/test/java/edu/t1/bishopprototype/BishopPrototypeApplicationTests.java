7737927e15bf74835e077df78e69726b164e2e17
melonegoist <408517@niuitmo.ru>
2025-07-21 12:27:39 +0300

    init bishop-prototype

---
+++ bishop-prototype/src/test/java/edu/t1/bishopprototype/BishopPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.t1.bishopprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BishopPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
